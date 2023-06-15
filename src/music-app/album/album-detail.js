a9c16146cd15a24d96a8a4829ee668a4d1e48023
yufeiz6 <yufeizhang0718@gmail.com>
2023-06-15 16:11:35 -0400

    add details

--- src/music-app/album/album-detail.js
+++ src/music-app/album/album-detail.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Container, Row, Col } from "react-bootstrap";
+import { BsHeart, BsBookmark } from "react-icons/bs";
 
 function AlbumDetails() {
   const { id } = useParams();
   const token = useSelector((state) => state.apiInfo.token);
   const [album, setAlbum] = useState(null);
-
   useEffect(() => {
     fetch(`https://api.spotify.com/v1/albums/${id}`, {
       headers: {
@@ -19,15 +20,31 @@ function AlbumDetails() {
       });
   }, [id, token]);
 
+  console.log(album);
   if (!album) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>{album.name}</h1>
-      <img src={album.images[0]?.url} alt={album.name} />
-      <p>{album.description}</p>
+    <div className="flex flex-col">
+      <Container>
+        <Row>
+          <Col md={6}>
+            <h1>{album.name}</h1>
+            <img src={album.images[0]?.url} alt={album.name} />
+          </Col>
+          <Col className="right-col" md={6}>
+             <h4>Album Type: {album.album_type}</h4>
+              <h4>Release Date: {album.release_date}</h4>
+              <h4>Total Tracks: {album.total_tracks}</h4>
+              <a href={album.external_urls.spotify} target="_blank" rel="noopener noreferrer">Spotify Link</a>
+            <Row>
+              <BsHeart className="heart-icon"/>
+              <BsBookmark className="bookmark-icon"/>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
