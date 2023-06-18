a8d916a2fa379f54c83a7aeaccbd03f96c38a316
Ge Xu <gexu13@outlook.com>
2023-06-18 13:22:27 -0700

    add auth-services

---
+++ src/music-app/services/auth-service.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:4000/api/users/";
+
+const api = axios.create({ withCredentials: true });
+
+export const register = async ({username, password}) => {
+    const response = await api.post(API_URL + "register", {
+        username,
+        password
+    });
+    return response.data;
+}
