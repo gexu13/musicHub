e398e69d420d3e081fed4b72a1cb3c5afd1d2f04
yufeiz6 <yufeizhang0718@gmail.com>
2023-06-20 16:44:12 -0700

    fix

--- src/music-app/profile-screen/review-detail.js
+++ src/music-app/profile-screen/review-detail.js
@@ -1,10 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import {useDispatch} from "react-redux";
+import {deleteReview} from "../services/reviews-thunks";
+import { AiOutlineCloseCircle } from 'react-icons/ai';
 import './review.css';
 
 function ReviewPiece({ review }) {
   const token = useSelector((state) => state.apiInfo.token);
   const [album, setAlbum] = useState(null);
+
+  const dispatch = useDispatch();
+  const deleteReviewHandler = (id) => {
+  dispatch(deleteReview(id));
+}
 
   useEffect(() => {
     fetch(`https://api.spotify.com/v1/albums/${review.albumId}`, {
@@ -25,28 +33,28 @@ function ReviewPiece({ review }) {
   return (
     <li className="list-group-item">
       <div className="review-container">
-        <div className="image">
-          <img
-            width={130}
-            className="rounded-3"
-            src={album.images[0]?.url}
-            alt={album.name}
-          />
-        </div>
-        <div className="content">
-          <div>
-          <div>
-            <h4>{album.name}</h4>
-            <div className="text-content">{review.review}</div>
+        <div className="review-details">
+          <div className="image">
+            <img
+              width={130}
+              className="rounded-3"
+              src={album.images[0]?.url}
+              alt={album.name}
+            />
           </div>
-          <div className="review-stat">
-            likes: {review.likes}
-          </div>
+          <div className="content">
+            <div>
+              <h4>{album.name}</h4>
+              <div className="text-content">{review.review}</div>
+              <div className="review-stat">likes: {review.likes}</div>
+            </div>
           </div>
         </div>
+        <AiOutlineCloseCircle className="close-icon"/>
       </div> 
     </li>
   );
+
 }
 
 export default ReviewPiece;
