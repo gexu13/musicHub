007e8a5d4f6e5e6176bf7a996d0f1cc6217bee27
yufeiz6 <yufeizhang0718@gmail.com>
2023-06-22 23:45:28 -0700

    render unique album for log in

--- src/music-app/home-screen/login-album.js
+++ src/music-app/home-screen/login-album.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import { AiOutlineCloseCircle } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
 
